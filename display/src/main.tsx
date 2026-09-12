7eddebaad59006a420ffb7065e7365642cedc25e
karlo.cavlovic <karlo.cavlovic@outlook.com>
2026-09-13 00:55:41 +0200

    feat(display): load seasonal styles

--- display/src/main.tsx
+++ display/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import "./styles.css";
+import "./seasonal.css";
 
 if (import.meta.env.VITE_OLYMPUS_KIOSK === "true") {
   document.documentElement.classList.add("olympus-kiosk");
